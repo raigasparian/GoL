384d808eb3bf7e96c9f177f949e52b4fd3932d75
raigasparian <raya.gasparyan.s@tumo.org>
2021-07-02 17:53:14 +0400

    avelacrac failer

--- Predater.js
+++ Predater.js
@@ -1,63 +1,4 @@
-
-
-
-
-
-
-
-class Buys {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.multiplay = 0
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ]
-    }
-
-    chooseCell(character) {
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-
-            }
-
-        }
-        return found;
-    }
-
-    mul() {
-        this.multiplay++;
-        var emptyCells = this.chooseCell(4);
-        var newCell = random(emptyCells);
-
-        console.log(emptyCells);
-        if (newCell && this.multiplay >= 8) {
-            var newX = newCell[0];
-            var newY = newCell[1];
-            matrix[newY][newX] = 4;
-
-            var newBuys = new Buys(newX, newY);
-            BuysArr.push(newBuys);
-            this.multiply = 0;
-        }
-    }
-
-}
-
-class BuysEater {
+class Predater {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -106,8 +47,8 @@ class BuysEater {
             var newY = newCell[1];
             matrix[newY][newX] = 3;
 
-            var newBuysEater = new BuysEater(newX, newY);
-            BuysEaterArr.push(newBuysEater);
+            var newPredater = new Predater(newX, newY);
+            PredaterArr.push(newPredater);
             this.multiply = 0;
         }
     }
@@ -134,7 +75,7 @@ class BuysEater {
     }
 
     eat() {
-        var emptyCells = this.chooseCell(4)
+        var emptyCells = this.chooseCell(2)
         var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
 
         if (newCell) {
@@ -146,9 +87,9 @@ class BuysEater {
             matrix[this.y][this.x] = 0
             this.x = newX
             this.y = newY
-            for (var i in BuysArr) {
-                if (newX == BuysArr[i].x && newY == BuysArr[i].y) {
-                    BuysArr.splice(i, 1)
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1)
                     break
                 }
             }
@@ -160,9 +101,9 @@ class BuysEater {
 
     die() {
         matrix[this.y][this.x] = 0;
-        for (var i in BuysEaterArr) {
-            if (this.x == BuysEaterArr[i].x && this.y == BuysEaterArr[i].y) {
-                BuysEaterArr.splice(i, 1);
+        for (var i in PredaterArr) {
+            if (this.x == PredaterArr[i].x && this.y == PredaterArr[i].y) {
+                PredaterArr.splice(i, 1);
                 break;
             }
         }
